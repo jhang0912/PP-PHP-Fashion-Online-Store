968fe5175b36a0be7a33dbf887f27ef2dd2963d0
jhang0912 <fueqaxvk@gmail.com>
2021-04-08 17:38:28 +0800

    1.home.php/forInspiration.php/home.css -以後端撰寫前端推薦商品元件-(.forInspiration) 2.forInspiration.js -前端動態效果-(.forInspiration輪播效果)

--- js/forInspiration.js
+++ js/forInspiration.js
@@ -1,9 +1,36 @@
-/* forInspiration html */
-$.get('api/forInspiration.php',function(res){
-  // $('.forInspiration').html(res);
-})
-
-$(document).ready(function(){
+$(document).ready(function () {
   /* 輪播效果*/
-
+  $('.forInspiration').slick({
+    arrows: true,
+    infinite: true,
+    autoplay: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    speed: 1000,
+    prevArrow: $('.prev2'),
+    nextArrow: $('.next2'),
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1
+        }
+      },
+    ]
+  });
 });
+/* 元件動態陰影效果 */
+$('.for-card').on('mouseover', function () {
+  $(this).removeClass('shadow-sm');
+  $(this).addClass('shadow');
+})
+$('.for-card').on('mouseout', function () {
+  $(this).removeClass('shadow');
+  $(this).addClass('shadow-sm');
+})
