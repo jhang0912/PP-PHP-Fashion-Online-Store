eff5a87d7a4f2d6b875f95ffa04fc7a301276e99
jhang0912 <fueqaxvk@gmail.com>
2021-04-26 20:55:28 +0800

    1.shoes.php -區塊完成依性別載入不同內容-

--- api/shoes.php
+++ api/shoes.php
@@ -1,22 +1,30 @@
 <div class="shoesShow d-flex flex-wrap justify-content-center align-items-center">
   <?php
   $sort = [
-    'Sneakers',
-    'loafers',
-    'Smart Shoes',
-    'C-ONE Capsule'
+    'men' => [
+      'Sneaker',
+      'Loafer',
+      'Smart Shoes',
+      'C-ONE Capsule'
+    ],
+    'women' => [
+      'Sneaker',
+      'Flat Shoes',
+      'Sandal',
+      'Boots'
+    ]
   ];
 
   for ($i = 0; $i < 4; $i++) {
-    ${$sort[$i]} = new getUrl2('collection', $sort[$i]);
+    ${$sort[$_GET['page']][$i]} = new getUrl2('shoes', $sort[$_GET['page']][$i]);
   ?>
     <div class="col-6 col-lg-4 col-xl-3 d-flex flex-wrap justify-content-center align-items-center p-1 wow animate__animated animate__fadeInUp">
-      <a class="d-flex flex-wrap justify-content-center align-items-center" href="?page=<?= (!empty($_GET['page']) && $_GET['page'] != 'home') ? $_GET['page'] : 'men' ?><?= ${$sort[$i]}->getNdUrl() ?>">
+      <a class="d-flex flex-wrap justify-content-center align-items-center" href="?page=<?= (!empty($_GET['page']) && $_GET['page'] != 'home') ? $_GET['page'] : 'men' ?><?= ${$sort[$_GET['page']][$i]}->getNdUrl() ?>">
         <div class="uk-inline-clip uk-transition-toggle" tabindex="0">
-          <img class="uk-transition-scale-up uk-transition-opaque" src="img/shoes/<?= $i ?>.jpg" alt="">
+          <img class="uk-transition-scale-up uk-transition-opaque" src="img/shoes/<?= (!empty($_GET['page']) && $_GET['page'] != 'home') ? $_GET['page'] : 'men' ?>/<?= $sort[$_GET['page']][$i] ?>.jpg" alt="<?= (!empty($_GET['page']) && $_GET['page'] != 'home') ? $_GET['page'] : 'men' ?> <?= $sort[$_GET['page']][$i] ?>">
         </div>
       </a>
-      <div class="co-title container h3 text-center <?=($i<4)?'m-3':'mt-3';?>"><?= $sort[$i] ?></div>
+      <div class="co-title container h3 text-center <?= ($i < 4) ? 'm-3' : 'mt-3'; ?>"><?= $sort[$_GET['page']][$i] ?></div>
     </div>
   <?php
   }
