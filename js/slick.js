63ec84ce412b4ee349939fa4994af674422b172d
jhang0912 <fueqaxvk@gmail.com>
2021-05-16 14:00:49 +0800

    1.slick.js -輪播功能開啟-

--- js/slick.js
+++ js/slick.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: false,
+    autoplay: true,
     autoplaySpeed: 6000,
     speed: 2000,
     prevArrow: $('.prev'),
